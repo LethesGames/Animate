86a8fe074cd67e6d8b692947c5fdd3683bbc8ba5
LethesGames <lethesgames@gmail.com>
2024-05-20 21:31:46 +0200

    Add Animations and logic to the player

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,9 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite : AnimatedSprite2D = get_node("AnimatedSprite2D")
+var animations_array = [0, 0, 0, 0] #Idle, Run, Jump, Slide
+
 	
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,10 +22,22 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction > 0:
+		if animations_array[1]:
+			animated_sprite.play("Run")
+		if animated_sprite.flip_h:
+			animated_sprite.flip_h = false
+		velocity.x = direction * SPEED
+	elif direction < 0:
+		if animations_array[1]:
+			animated_sprite.play("Run")
+		if not animated_sprite.flip_h:
+			animated_sprite.flip_h = true
 		velocity.x = direction * SPEED
 	else:
+		if animations_array[0]:
+			animated_sprite.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
